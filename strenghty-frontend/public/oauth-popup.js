ccac3ea233478a07a33971452a8112c6636c363a
Test <nr14082005@gmail.com>
2026-03-03 02:12:33 +0530

    Fix OAuth popup on Vercel: static callback JS and rewrite exclusions

---
+++ strenghty-frontend/public/oauth-popup.js
@@ -0,0 +1,53 @@
+(function () {
+  function relay() {
+    try {
+      var hash = new URLSearchParams(window.location.hash.replace(/^#/, ""));
+      var accessToken = hash.get("access_token");
+      var idToken = hash.get("id_token");
+      if (!accessToken) return;
+
+      try {
+        if (window.opener) {
+          window.opener.postMessage(
+            {
+              type: "supabase-oauth-result",
+              accessToken: accessToken,
+              idToken: idToken || null,
+            },
+            window.location.origin,
+          );
+        }
+      } catch (e) {}
+
+      try {
+        localStorage.setItem(
+          "supabase:oauth_result",
+          JSON.stringify({
+            accessToken: accessToken,
+            idToken: idToken || null,
+          }),
+        );
+      } catch (e) {}
+
+      try {
+        var ch = new BroadcastChannel("supabase_oauth");
+        ch.postMessage({
+          type: "supabase-oauth-result",
+          accessToken: accessToken,
+          idToken: idToken || null,
+        });
+        ch.close();
+      } catch (e) {}
+    } catch (e) {}
+
+    try {
+      window.close();
+    } catch (e) {}
+  }
+
+  relay();
+  var timer = setInterval(relay, 250);
+  setTimeout(function () {
+    clearInterval(timer);
+  }, 5000);
+})();
