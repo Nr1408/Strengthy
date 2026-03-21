b849155e25bef33b704f26361a029ba3d44df35a
Test <nr14082005@gmail.com>
2026-03-22 01:59:03 +0530

    1

---
+++ strenghty-frontend/capacitor.config.ts
@@ -0,0 +1,46 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig & { tsSplashScreen?: any } = {
+  appId: 'com.strengthy.app',
+  appName: 'Strengthy',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https',
+    hostname: 'localhost',
+    cleartext: true,
+  },
+  overrideUserAgent:
+    'Mozilla/5.0 (Linux; Android 13; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36',
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false,
+      backgroundColor: '#0f0f0f',
+      style: 'LIGHT',
+    },
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      androidClientId:
+        '682920475586-donl8vohortm58eqb2ulbcg48tf2uhnp.apps.googleusercontent.com',
+      serverClientId:
+        '682920475586-h98muldc2oqab094un02au2k8c5cj9i1.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    },
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: '#0a0a0a',
+      androidSplashResourceName: 'splash',
+      showSpinner: false,
+    },
+  },
+  tsSplashScreen: {
+    launchShowDuration: 1500,
+    launchAutoHide: true,
+    backgroundColor: '#0a0a0a',
+    androidSplashResourceName: 'splash',
+    showSpinner: false,
+  },
+};
+
+export default config;
